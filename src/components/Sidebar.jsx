3e14b68ddf00413950980d79aba7d2a63d3bede4
ChristieCardoso <christiecardoso@hotmail.com>
2023-10-25 18:07:04 -0300

    edit slidebar.sass

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,16 @@
+import Avatar from '../img/chs.png';
+
+import '../styles/components/slidebar.sass';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside id="sidebar">
+      <img src={Avatar} alt="chs" />
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">Download do Currículo</a>
+    </aside>
   )
 }
 
