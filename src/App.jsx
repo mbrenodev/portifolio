902305fe4b30dc8eaeb0ac5f02c2d8e2dd1a04ed
ChristieCardoso <christiecardoso@hotmail.com>
2023-11-08 19:56:58 -0300

    reaproveitando components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './router';
 
+import { Outlet } from 'react-router-dom';
 import './styles/app.sass';
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />      
-    </BrowserRouter>
+    <div className='App'>
+      <p>oi</p>
+      <Outlet />
+
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './App.jsx';
 
 import "./styles/main.sass";
 
+//1- conf router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { App } from './App';
+import { Home } from './pages/Home/Home';
+import { Projetos } from './pages/Projects/Projetos';
+
+// import Home from './pages/Home/Home.jsx';
+// import Project from './pages/Projects/Projetos.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />
+//   },
+//   {
+//     path: "/project",
+//     element: <Project />
+//   },
+// ]);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "project",
+        element: <Projetos />
+      }
+    ]
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
