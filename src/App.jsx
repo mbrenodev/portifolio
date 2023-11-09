bda860cd8f1083a4d0c631e41021a0f4ed237317
ChristieCardoso <christiecardoso@hotmail.com>
2023-11-09 13:41:22 -0300

    navegar entre as páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import { Outlet } from 'react-router-dom';
 import './styles/app.sass';
+import { NavBar } from './components/NavBar/NavBar';
 
 export function App() {
   return (
-    <div className='App'>
-      <p>oi</p>
+    <div id='portfolio'>
+      <NavBar />
       <Outlet />
-
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import MainContent from "../../components/MainContent/MainContent"
-import { NavBar } from "../../components/NavBar/NavBar"
+
 import Sidebar from "../../components/Sidebar/Sidebar"
 
 export const Home = () => {
   return (
     <div id='portfolio'>
-      <NavBar />
+      
       <Sidebar />
       <MainContent />
     </div>
