992826fa13047a8aaeca8fe30eb4485c9e335dea
ChristieCardoso <christiecardoso@hotmail.com>
2023-11-10 21:10:56 -0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
 
 import './styles/app.sass';
+import { Footer } from './components/Footer/Footer';
 
 export function App() {
   return (
     <div id='portfolio'>
       <NavBar />
       <Outlet />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+
+export const Footer = () => {
+  return (
+    <div>© 2023 Todos os direitos reservados.</div>
+  )
+}
