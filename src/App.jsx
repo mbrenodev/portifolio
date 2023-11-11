393abc9940fb9d8e7f2be5017c4ae4db342038f5
ChristieCardoso <christiecardoso@hotmail.com>
2023-11-11 19:30:19 -0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom';
 
 import { NavBar } from './components/NavBar/NavBar';
+import { Footer } from './components/Footer/Footer';
 
 import './styles/app.sass';
-import { Footer } from './components/Footer/Footer';
 
 export function App() {
   return (
