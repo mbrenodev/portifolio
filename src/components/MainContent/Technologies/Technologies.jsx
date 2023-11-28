d6624afadd6847ce5ef0c706b6fbea4ebe012d9c
ChristieCardoso <christiecardoso@hotmail.com>
2023-11-27 21:35:44 -0300

    edit home

--- src/components/MainContent/Technologies/Technologies.jsx
+++ src/components/MainContent/Technologies/Technologies.jsx
@@ -11,33 +11,32 @@ import {
 import "./Technologies.scss";
 
 const technologies = [
-  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-  { id: "css", name: "CSS3", icon: <DiCss3 /> },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-  { id: "react", name: "React", icon: <DiReact /> },
+  { id: "html", name: "HTML5", age: "3 Anos", icon: <DiHtml5 /> },
+  { id: "css", name: "CSS3", age: "3 Anos", icon: <DiCss3 /> },
+  { id: "js", name: "JavaScript", age: "3 Anos", icon: <DiJsBadge /> },
+  { id: "node", name: "Node.js", age: "1 Ano", icon: <DiNodejsSmall /> },
+  { id: "mysql", name: "MySQL", age: "1 Ano", icon: <DiMysql /> },
+  { id: "react", name: "React", age: "1 Ano", icon: <DiReact /> },
 ];
 
 export const Technologies = () => {
   return (
     <section className="technologies-container">
-      <h2>Tecnologias</h2>
+      <h2>Conhecimentos</h2>
       <div className="technologies-grid">
         {technologies.map((tech) => (
           <div className="technology-card" id={tech.id} key={tech.id}>
             {tech.icon}
             <div className="technology-info">
               <h3>{tech.name}</h3>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+              <p>{tech.age}</p>
             </div>
           </div>
         ))}
       </div>
       <section className="projects-container">
-        <h2>Projetos</h2>
-        <Link to="/project" className="btn">
-          Ver Projetos
+        <Link to="/about" className="btn">
+          Veja Mais
         </Link>
       </section>
     </section>
