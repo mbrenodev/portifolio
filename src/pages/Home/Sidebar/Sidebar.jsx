90796538aadc13f9a88df71d5c45b261ba0302af
ChristieCardoso <christiecardoso@hotmail.com>
2023-11-28 20:16:13 -0300

    edit home

--- src/pages/Home/Sidebar/Sidebar.jsx
+++ src/pages/Home/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 
-import { Information } from "./Information/Information.jsx";
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
 
 import Avatar from "../../../img/chs.png";
 
@@ -10,7 +10,29 @@ const Sidebar = () => {
     <aside id="sidebar">
       <img src={Avatar} alt="Chs" />
       <p className="title">Desenvolvedor</p>
-      <Information />
+      <section id="information">
+        <div className="info-card">
+          <AiFillPhone id="phone-icon" />
+          <div>
+            <h3>Telefone</h3>
+            <p>(31)99999-9999</p>
+          </div>
+        </div>
+        <div className="info-card">
+          <AiOutlineMail id="email-icon" />
+          <div>
+            <h3>E-mail</h3>
+            <p>christie@gmail.com</p>
+          </div>
+        </div>
+        <div className="info-card">
+          <AiFillEnvironment id="pin-icon" />
+          <div>
+            <h3>Localização</h3>
+            <p>Contagem / MG</p>
+          </div>
+        </div>
+      </section>
       <a href="#" className="btn">
         Download currículo
       </a>
