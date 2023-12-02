39cf8990c9983bc68ef967d495c2f2d4832c47b2
ChristieCardoso <christiecardoso@hotmail.com>
2023-12-01 22:22:11 -0300

    edit navbar

--- src/pages/Home/WorkExperience/WorkExperience.jsx
+++ src/pages/Home/WorkExperience/WorkExperience.jsx
@@ -7,19 +7,19 @@ export const WorkExperience = () => {
 
       <div className='WorkExperienceBody'>
         <div className='WorkExperienceCard'>
-          <h3 className='WorkExperienceTitle'>Desenvolvedor Web</h3>
+          <h3 className='WorkExperienceTitle'>Desenvolvedor WordPress</h3>
           <span className='WorkExperienceAge'>out 2023 ° No Momento ° (3 meses)</span>
-          <p className='WorkExperienceText'>Desenvolvimento e manutenção de interfaces utilizando React, Next, Tailwind, Typescript e Figma. Para o planejamento dos sprints, é utilizado o Jira.</p>
+          <p className='WorkExperienceText'>Desenvolvimento de funcionalidades personalizadas usando linguagens como PHP, HTML, CSS e JavaScript. Monitoramento e análise de métricas de desempenho do site, como tempo de carregamento, taxa de rejeição e conversões.</p>
           <div className='WorkExperienceCompe'>Competência</div>
-          <span className='WorkExperienceTech'>WordPress</span>
+          <span className='WorkExperienceTech'>WordPress | JavaScript | HTML5 | CSS3</span>
         </div>
 
         <div className='WorkExperienceCard'>
-          <h3 className='WorkExperienceTitle'>Desenvolvedor WordPress</h3>
-          <span className='WorkExperienceAge'>out 2023 ° No Momento ° (3 meses)</span>
-          <p className='WorkExperienceText'>Desenvolvimento e manutenção de interfaces utilizando React, Next, Tailwind, Typescript e Figma. Para o planejamento dos sprints, é utilizado o Jira.</p>
+          <h3 className='WorkExperienceTitle'>Freelancer em Desenvolvimento Web</h3>
+          <span className='WorkExperienceAge'>° No Momento °</span>
+          <p className='WorkExperienceText'>Atuo como freelancer no desenvolvimento e manutenção de interfaces utilizando React, Next.js e Tailwind CSS. Tenho conhecimentos sólidos em JavaScript, HTML e CSS, além de experiência com o uso do Figma para criação de designs. Trabalho de forma independente, entregando projetos com qualidade e dentro dos prazos estabelecidos.</p>
           <div className='WorkExperienceCompe'>Competência</div>
-          <span className='WorkExperienceTech'>WordPress</span>
+          <span className='WorkExperienceTech'>React | Next.js | Tailwind CSS</span>
         </div>
       </div>
 
