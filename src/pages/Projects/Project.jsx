4fa0793ccdc22f558fae42839cd890010ca0ac89
Márcio Cardoso <mbreno.dev@gmail.com>
2023-12-02 20:36:38 -0300

    Update Project.jsx
    Correção

--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -1,26 +1,19 @@
 import ProjectCard from './ProjectCard/ProjectCard';
-
 import Project from '../../data/projects'
-
 import './Project.scss';
 
 export const Projects = () => {
   return (
     <div className='projectContainer'>
-      <h1 className='ProjectTitle'>
-        Meus Projetos
-      </h1>
-      <h3>
-        Aqui você poderá ver alguns dos trabalhos que eu desenvolvi. Navegue à vontade e explore os projetos para ver como foram criados, as tecnologias utilizadas e as funcionalidades implementadas.
-      </h3>
+      <h1 className='ProjectTitle'>Meus Projetos</h1>
+      <h3>Aqui você poderá ver alguns dos trabalhos que eu desenvolvi. Navegue à vontade e explore os projetos para ver como foram criados, 
+        as tecnologias utilizadas e as funcionalidades implementadas.</h3>
 
       <section className='projects'>
         {Project.map((props) => (
           <ProjectCard key={props.id} {...props} />
         ))}
-
       </section>
-
     </div>
   );
 };
